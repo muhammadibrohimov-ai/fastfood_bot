681b161b90d737a4d95ae1d63a235ddb5242b118
Muhammadjon Ibrohimov <ibrokhimovmiu@gmail.com>
2025-09-15 21:21:35 +0500

    added sqlite3

--- database/connect.py
+++ database/connect.py
@@ -1,16 +1,68 @@
 import psycopg as sql
 
+import sqlite3 as sql
+
 from environs import Env
 env = Env()
 env.read_env()
 
-def get_connection():
-    return sql.connect(
-        dbname = env.str("DB_NAME"),
-        user = env.str("USERNAME_SQL"),
-        password = env.str("PASSWORD"),
-        host = env.str("HOST"),
-        port = env.str("PORT"),
-        autocommit=True
-    )
+# def get_connection():
+#     return sql.connect(
+#         dbname = env.str("DB_NAME"),
+#         user = env.str("USERNAME_SQL"),
+#         password = env.str("PASSWORD"),
+#         host = env.str("HOST"),
+#         port = env.str("PORT"),
+#         autocommit=True
+#     )
 
+def get_connection():
+    return sql.connect("fastfood.db",)
+
+users = """
+CREATE TABLE users (
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    chat_id  INTEGER UNIQUE,
+    fullname VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    phone    VARCHAR(100) UNIQUE,
+    long     VARCHAR(50),
+    lat      VARCHAR(50),
+    is_admin INTEGER NOT NULL,
+    UNIQUE(chat_id),
+    UNIQUE(phone)
+);
+
+"""
+
+foods = '''
+CREATE TABLE foods (
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    name        VARCHAR(200),
+    image       VARCHAR(250),
+    price       INTEGER,
+    quantity    INTEGER,
+    description VARCHAR(500) NOT NULL DEFAULT 'This is food'
+);
+'''
+
+orders = '''
+CREATE TABLE orders (
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    food_id    INTEGER NOT NULL,
+    user_id    INTEGER NOT NULL,
+    quantity   INTEGER NOT NULL,
+    price      DECIMAL(12,2) NOT NULL,
+    status     VARCHAR(20) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (food_id) REFERENCES foods(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+'''
+
+# for table in [users, foods, orders]:
+#     with get_connection() as db:
+#         dbc = db.cursor()
+#         dbc.execute(table)
+#         db.commit() 
+        
--- database/query.py
+++ database/query.py
@@ -2,46 +2,50 @@ from database.connect import get_connection
 
 def get_users(chat_id):
     with get_connection() as db:
-        with db.cursor() as dbc:
-            query = f"SELECT * FROM users WHERE chat_id = {chat_id}"
-            dbc.execute(query)
-            return dbc.fetchone()
+        dbc = db.cursor()
+        query = f"SELECT * FROM users WHERE chat_id = {chat_id}"
+        dbc.execute(query)
+        return dbc.fetchone()
 
 def add_to_table(table_name, **kwargs):
     keys = ', '.join(kwargs.keys())
-    placeholders = ', '.join(['%s'] * len(kwargs))
+    placeholders = ', '.join(['?'] * len(kwargs))
     values = tuple(kwargs.values())
 
     query = f"INSERT INTO {table_name} ({keys}) VALUES ({placeholders});"
 
     try:
         with get_connection() as db:
-            with db.cursor() as dbc:
-                dbc.execute(query, values)
-                return True
+            dbc = db.cursor()
+            dbc.execute(query, values)
+            db.commit()
+            return True
+            
     except Exception as e:
         print(f"Error inserting into {table_name}: {e}")
         return False
 
 def get_foods():
     with get_connection() as db:
-        with db.cursor() as dbc:
-            dbc.execute("SELECT * FROM foods;")
-            return dbc.fetchall()
+        dbc = db.cursor()
+        dbc.execute("SELECT * FROM foods;")
+        return dbc.fetchall()
         
 def get_specific_food(id):
     with get_connection() as db:
-        with db.cursor() as dbc:
-            dbc.execute(f'SELECT * FROM foods WHERE id = {id};')
-            return dbc.fetchone()
+        dbc = db.cursor()
+        dbc.execute(f'SELECT * FROM foods WHERE id = {id};')
+        return dbc.fetchone()
         
 def change_table(query):
     try:
         with get_connection() as db:
-            with db.cursor() as dbc:
-                dbc.execute(query)
-                db.commit()
+            dbc = db.cursor()
+            dbc.execute(query)
+            db.commit()
         return True
+    
+    
     except Exception as e:
         print(e)
         return False
@@ -49,10 +53,12 @@ def change_table(query):
 def get_order_food(status : str):
     try:
         with get_connection() as db:
-            with db.cursor() as dbc:
-                dbc.execute(f"SELECT o.id AS order_id, f.name AS food_name, u.id AS user_id, f.price AS food_price, o.quantity AS order_quantity, (o.quantity * f.price) AS total_price FROM orders o JOIN foods f ON o.food_id = f.id JOIN users u ON o.user_id = u.id WHERE status = '{status}';")
-                data = dbc.fetchall()
+            dbc = db.cursor()
+            dbc.execute(f"SELECT o.id AS order_id, f.name AS food_name, u.id AS user_id, f.price AS food_price, o.quantity AS order_quantity, (o.quantity * f.price) AS total_price FROM orders o JOIN foods f ON o.food_id = f.id JOIN users u ON o.user_id = u.id WHERE status = '{status}';")
+            data = dbc.fetchall()
         return data
+    
+    
     except Exception as e:
         print(e)
         return None
